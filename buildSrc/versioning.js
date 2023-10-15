64a46b098baa44963f89f1970a9007162374912b
WasabiThumb <wasabithumbs@gmail.com>
2023-10-15 03:44:58 -0400

    Git versioning

--- buildSrc/versioning.js
+++ buildSrc/versioning.js
@@ -1,5 +1,6 @@
 const fs = require("fs/promises");
 const path = require('path');
+const buildscript = require("../package.json");
 
 
 // A replacement for the parts of the "nodegit" module that we actually use.
@@ -57,3 +58,43 @@ const Git = (() => {
         }
     };
 })();
+
+// injects the version into src/util/version.ts
+const setVersion = (async (version) => {
+    const fn = path.resolve(__dirname, "..", "src", "util", "version.ts");
+    const f = await fs.open(fn, 'r');
+    const tmp = await fs.open(fn + ".tmp", 'w');
+
+    for await (const line of f.readLines()) {
+        if (/^(.*)LIB_VERSION(.*)/i.test(line)) {
+            await tmp.write(`export const LIB_VERSION: string = "${version}";\n`);
+        } else {
+            await tmp.write(`${line}\n`);
+        }
+    }
+
+    await f.close();
+    await tmp.close();
+
+    await fs.rename(fn + ".tmp", fn);
+    await fs.rm(fn + ".tmp").catch(() => {});
+});
+
+// queries, updates, and returns the version data
+const version = (() => {
+    return Git.Repository.open(path.resolve(__dirname, "..")).then(async (repo) => {
+        const branch = await repo.getCurrentBranch();
+        const commit = await repo.getHeadCommit();
+        if (branch.shorthand().toLowerCase() === "master") {
+            return { mode: 'production', devtool: false, version: buildscript.version };
+        } else {
+            const postfix = commit.sha().slice(0, 7);
+            return { mode: 'development', devtool: 'source-map', version: `${buildscript.version}-git-${postfix}` };
+        }
+    }).then(async (dat) => {
+        await setVersion(dat.version);
+        return dat;
+    });
+});
+
+module.exports = version;
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,42 @@
 const path = require("path");
+const webpack = require("webpack");
 const CopyPlugin = require("copy-webpack-plugin");
+const version = require("./buildSrc/versioning");
 
-module.exports = {
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules|static|dist|private/
-            }
-        ]
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                "static/index.html",
-                "static/.key.private",
-                { from: "static/assets", to: path.resolve(__dirname, 'dist', 'assets') }
+
+module.exports = (async () => {
+    const vd = await version();
+    return {
+        entry: './src/index.ts',
+        mode: vd.mode,
+        devtool: vd.devtool,
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules|static|dist|private/
+                }
             ]
-        })
-    ],
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
-}
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    "static/index.html",
+                    "static/.key.private",
+                    { from: "static/assets", to: path.resolve(__dirname, 'dist', 'assets') }
+                ]
+            }),
+            new webpack.BannerPlugin({
+                banner: `Portfolio v. ${vd.version}\nWith <3 By Wasabi`
+            })
+        ],
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js']
+        },
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        }
+    };
+});
